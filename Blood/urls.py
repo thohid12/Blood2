6964e33b1398c05881c79102b1e42d875d65ba62
thohid12 <145980877+thohid12@users.noreply.github.com>
2024-09-27 08:53:02 +0600

    Update urls.py

--- Blood/urls.py
+++ Blood/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path('',include('fblood.urls')),
     path('admin/', admin.site.urls),
 ]
-urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #extra for deploying
 #urlpatterns+=staticfiles_urlpatterns()
