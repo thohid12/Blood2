8e19f5081f49af8607f8324cb723fe6100c515ed
thohid12 <145980877+thohid12@users.noreply.github.com>
2024-09-29 17:39:41 +0600

    Update build.sh migration

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py makemigrations && python manage.py migrate
+
 if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
